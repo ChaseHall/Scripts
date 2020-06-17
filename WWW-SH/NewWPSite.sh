6c73f8122206baabc0b35ce51443cd2acab0c811
Chase <ch@chasehall.net>
2020-06-17 17:17:01 +0000

    Update 'WWW-SH/NewWPSite.sh'

--- WWW-SH/NewWPSite.sh
+++ WWW-SH/NewWPSite.sh
@@ -1,32 +1,32 @@
 #!/bin/bash
 
-# Define stuff so I can change MIAB info easier.
-# Change ddns, 2, 3, 4 aswell.
-argxd=""
+# Args
+miabp1="curl -X PUT --user"
 miabemail=""
-miabpass=""
-link=""
+miabpw=""
+miabp2="https://mail.nebulahost.us/admin/dns/custom"
+dbpw=""
+# Args (Not user gen'd)
+servn=$1
+dbname=$(echo "${servn//.}")
+rndpw=$(bash /root/pwgen.sh)
 
 
-# Check root
+# Checking things...
 if [ "$(whoami)" != 'root' ]; then
 echo "You have to execute this script as root user"
 exit 1;
 fi
-# Take variable from menu.sh and use it.
-servn=$1
-# Check if already exists
 if ! mkdir -p /var/www/$servn; then
 echo "Already Exists."
 exit 1;
 else
+  # Begin installing...
   chown -R www-data:www-data /var/www/$servn/
-
-  echo "$argxd $miabemail:$miabpass $link/$servn" >> /root/ddns.sh
+  echo "$miabp1 $miabemail:$miabpw $miabp2/$servn" >> /root/ddns.sh
   echo "sleep 1" >> /root/ddns.sh
-  $argxd $miabemail:$miabpass $link/$servn
-  $argxd $miabemail:$miabpass $link/$servn
-  
+  $miabp1 $miabemail:$miabpw $miabp2/$servn
+  $miabp1 $miabemail:$miabpw $miabp2/$servn
 echo "
 # BEGIN $servn
 
@@ -42,6 +42,11 @@ Redirect permanent / https://$servn/
     ServerAdmin ch@chasehall.net
     ServerName $servn
     DocumentRoot /var/www/$servn
+<Directory /var/www/$servn>
+Order Deny,Allow #$servn
+Deny from all #$servn
+Allow from 127.0.0.1 #$servn
+</Directory>
 #Include /etc/letsencrypt/options-ssl-apache.conf
 #SSLCertificateFile /etc/letsencrypt/live/$servn/fullchain.pem
 #SSLCertificateKeyFile /etc/letsencrypt/live/$servn/privkey.pem
@@ -50,39 +55,26 @@ Redirect permanent / https://$servn/
 
 # END $servn
 ">> /etc/apache2/sites-available/www.conf
-
 sudo systemctl restart apache2
 sudo certbot certonly --apache -d $servn
 sed -i '/#Include \/etc\/letsencrypt\/options-ssl-apache.conf/s/^# *//' /etc/apache2/sites-available/www.conf
 sed -i '/#SSLCertificateFile \/etc\/letsencrypt\/live\/'$servn'\/fullchain.pem/s/^# *//' /etc/apache2/sites-available/www.conf
 sed -i '/#SSLCertificateKeyFile \/etc\/letsencrypt\/live\/'$servn'\/privkey.pem/s/^# *//' /etc/apache2/sites-available/www.conf
 sudo systemctl restart apache2
-
 cd /var/www/$servn/
 wget http://wordpress.org/latest.tar.gz
 tar -xzvf latest.tar.gz
 mv wordpress/* .
 rm -r wordpress/
 rm latest.tar.gz
-#cd wp-content/themes/
-#git clone https://github.com/bakesaled/super-minimal.git
-#cd .. && cd ..
-dbname=$(echo "${servn//.}")
-rndpw=$(bash /root/pwgen.sh)
-#rndpw2=$(bash pwgen.sh)
-mysql -uroot -p[] -e "CREATE DATABASE $dbname;"
-mysql -uroot -p[] -e "CREATE USER $dbname@localhost IDENTIFIED BY '$rndpw';"
-mysql -uroot -p[] -e "GRANT ALL PRIVILEGES ON $dbname.* TO '$dbname'@'localhost';"
-mysql -uroot -p[] -e "FLUSH PRIVILEGES;"
-clear
+mysql -uroot -p$dbpw -e "CREATE DATABASE $dbname;"
+mysql -uroot -p$dbpw -e "CREATE USER $dbname@localhost IDENTIFIED BY '$rndpw';"
+mysql -uroot -p$dbpw -e "GRANT ALL PRIVILEGES ON $dbname.* TO '$dbname'@'localhost';"
+mysql -uroot -p$dbpw -e "FLUSH PRIVILEGES;"
 wp config create --dbname=$dbname --dbuser=$dbname --dbpass=$rndpw --allow-root
 wp core install --url=https://$servn --title=$servn --admin_user=Chase --admin_password=$rndpw --admin_email=ch@chasehall.net --allow-root
-#wp theme activate super-minimal --allow-root
-wp plugin install maintenance --allow-root
-wp plugin activate maintenance --allow-root
-#wp plugin install two-factor-authentication --allow-root
+wp plugin install maintenance --activate --allow-root
 wp plugin install daggerhart-openid-connect-generic --allow-root
-sudo chown -R www-data:www-data /var/www/$servn
 rm -rf /wp-content/themes/twentyfourteen
 rm -rf /wp-content/themes/twentyfifteen
 rm -rf /wp-content/themes/twentysixteen
@@ -94,31 +86,26 @@ wp plugin delete hello --allow-root
 wp rewrite structure '/%postname%/' --allow-root
 wp option update default_comment_status closed --allow-root
 wp post create --post_type=page --post_status=publish --post_title='Home' --allow-root
-wp plugin install all-404-redirect-to-homepage --allow-root
-wp plugin install autoptimize --allow-root
-wp plugin install insert-headers-and-footers --allow-root
-wp plugin install better-wp-security --allow-root
+wp plugin install all-404-redirect-to-homepage --activate --allow-root
+wp plugin install autoptimize --activate --allow-root
+wp plugin install insert-headers-and-footers --activate --allow-root
+wp plugin install better-wp-security --activate --allow-root
 wp plugin install mainwp-child --allow-root
-wp plugin install redirection --allow-root
-wp plugin install wp-super-cache --allow-root
-wp plugin install wordpress-seo --allow-root
-sudo chown -R www-data:www-data /var/www/$servn
-WP_ROOT=/var/www/$servn
-#find ${WP_ROOT} -exec chown www-data:www-data {} \;
-#find ${WP_ROOT} -type d -exec chmod 755 {} \;
-#find ${WP_ROOT} -type f -exec chmod 644 {} \;
-#chgrp www-data ${WP_ROOT}/wp-config.php
-#chmod 660 ${WP_ROOT}/wp-config.php
-#find ${WP_ROOT}/wp-content -exec chgrp www-data {} \;
-#find ${WP_ROOT}/wp-content -type d -exec chmod 775 {} \;
-#find ${WP_ROOT}/wp-content -type f -exec chmod 755 {} \;
-#sudo chmod 666 /var/www/$servn/wp-cron.php
-sudo chown -R www-data:www-data /var/www/$servn
+wp plugin install redirection --activate --allow-root
+wp plugin install wp-super-cache --activate --allow-root
+wp plugin install wordpress-seo --activate --allow-root
+wp plugin install adminimize --activate --allow-root
+wp plugin install capability-manager-enhanced --activate --allow-root
+chown -R www-data:www-data /var/www/$servn/
+sed -i "s/Order Deny,Allow #$servn/#Order Deny,Allow #$servn/g" /etc/apache2/sites-available/www.conf
+sed -i "s/Deny from all #$servn/#Deny from all #$servn/g" /etc/apache2/sites-available/www.conf
+sed -i "s/Allow from 127.0.0.1 #$servn/#Allow from 127.0.0.1 #$servn/g" /etc/apache2/sites-available/www.conf
+sudo systemctl restart apache2
 clear
 echo =======================================================================
 echo Setup Done.
 echo
-echo Add $servn to: https://cmh.pw/dnsm
+echo Add $servn to DNS Host Mapping on modem.
 echo
 echo
 echo Your WP Login:
@@ -127,28 +114,14 @@ echo Chase
 echo $rndpw
 echo
 echo
-echo Activate ALL plugins [besides MainWP, unless you are ready.]
-echo Configure Plugins in this order.
+echo Go configure all the plugins now.
+echo Activate MainWP when ready, and OpenID-Connect.
 echo
-# All these links are dead, whoops.
-echo     SSO Plugin - https://cmh.pw/h84a-k
-echo     Redirection
-echo     iThemes Security - https://cmh.pw/pl5lvo
-echo     MainWP Child - https://cmh.pw/8bvcp8
-echo     Autoptimize
-echo     WP Super Cache
-echo     Insert Headers and Footers - https://cmh.pw/ihf
-echo     Yoast SEO - https://cmh.pw/8c84yk
-echo     iThemes Security - https://cmh.pw/pl5lvo
 echo
-echo Install additional plugins.
-echo     Adminimize
+echo Install any additional plugins.
+echo     WP Mail SMTP
 echo     Contact Form 7
-echo     Email Subscribers and Newsletters - https://cmh.pw/m74ag5
-echo     PublishPress Capabilities
+echo     Email Subscribers and Newsletters
 echo     Ultimate Member
 echo     WooCommerce
-echo     WP Mail SMTP - https://cmh.pw/2mpa17
-echo
-echo Add new theme.
 fi
