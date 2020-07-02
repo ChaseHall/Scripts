c8fe8b4087754ec08f7a95c409fc3decd046f0ff
Chase Hall <ch@chasehall.net>
2020-07-02 02:21:02 +0000

    Update 'Plex/fixperms.sh'

--- Plex/fixperms.sh
+++ Plex/fixperms.sh
@@ -2,3 +2,4 @@ sudo chown -R root:root /media/easystore/Movies
 sudo chown -R root:root "/media/easystore/TV Shows"
 sudo chmod 777 -R "/media/easystore/Movies"
 sudo chmod 777 -R "/media/easystore/TV Shows"
+sudo chmod 777 -R "/media/easystore/TorrentDLs"
