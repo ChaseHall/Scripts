9a1ef1361b62d29a144e9e12781859e6e06e90aa
Chase Hall <ch@chasehall.net>
2020-07-06 01:31:23 +0000

    Super Basic start.

---
+++ New-Machine/setup.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+
+# This assumes you already have a Parrot OS Security Edition install up and running, with FDE & a sudo user account.
+# https://parrotlinux.org/download/
+
+# Whats missing:
+# General customization
+# Any form of settings (power/display)
+# Plex (AppImage)
+
+sudo apt install vim curl apt-transport-https -y
+sudo apt update && sudo apt install fail2ban -y && sudo systemctl enable fail2ban && sudo systemctl start fail2ban
+echo "set mouse-=a" >> ~/.vimrc
+echo "
+# Chase Aliases
+alias update="sudo apt update && sudo apt upgrade -y && sudo apt dist-upgrade -y && sudo snap refresh"
+alias www="ssh -p1000 root@chasehall.net"
+alias invoiceninja="ssh -p1001 root@chasehall.net"
+alias dockerce="ssh -p1002 root@chasehall.net"
+alias plex="ssh -p1003 root@chasehall.net"
+alias invidious="ssh -p1004 root@chasehall.net"
+alias proxmox="ssh -p1005 root@chasehall.net"
+alias mail="ssh root@mail.nebulahost.us"
+alias pi="ssh -p1000 pi@aidenpi.cmh.pw"
+alias usb="ssh chase@lw833.usbx.me"
+alias mc="java -jar ~/Nextcloud/1_Personal/Programs/TLauncher-2.69.jar && exit"
+alias easystore="sudo sshfs -o allow_other,default_permissions,IdentityFile=/home/user/.ssh/id_rsa -p 1003 root@chasehall.net:/ /media/remotemount"
+alias mac="ssh Hall@192.168.86.182"
+#alias usb="sudo cryptsetup luksOpen /dev/sdb1 cusb && sudo mount /dev/mapper/cusb /mnt/usb/"
+" >> ~/.bashrc
+echo "
+deb https://assets.checkra.in/debian /
+deb http://ppa.launchpad.net/yubico/stable/ubuntu cosmic main 
+deb-src http://ppa.launchpad.net/yubico/stable/ubuntu cosmic main
+deb [arch=amd64] https://brave-browser-apt-release.s3.brave.com/ bionic main
+deb http://download.opensuse.org/repositories/home:/ivaradi/Debian_10/ /
+" >> /etc/apt/sources.list
+echo "
+deb https://mirrors.ocf.berkeley.edu/parrot/ rolling main contrib non-free
+## stable repository
+deb https://deb.parrot.sh/parrot rolling main contrib non-free
+deb https://deb.parrot.sh/parrot rolling-security main contrib non-free
+" >> /etc/apt/sources.list.d/parrot.list
+sudo apt update
+sudo snap install spotify
+sudo snap install discord
+sudo apt install -y vscodium tor filezilla hexchat calibre brave-browser gnupg2 gnupg-agent pinentry-curses scdaemon pcscd yubioath-desktop libpam-yubico yubikey-manager-qt yubikey-manager yubikey-personalization yubikey-personalization-gui
