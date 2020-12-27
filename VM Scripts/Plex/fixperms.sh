9eea6bf8ce136937cc83a742af83a1a5496acc26
Chase <chase@noreply.localhost>
2020-12-27 00:29:53 +0000

    Are my commits fixed now?

--- VM Scripts/Plex/fixperms.sh
+++ VM Scripts/Plex/fixperms.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-sudo chown -R root:root /media/easystore/Movies
+sudo chown -R root:root "/media/easystore/Movies"
 sudo chown -R root:root "/media/easystore/TV Shows"
 sudo chmod 777 -R "/media/easystore/Movies"
 sudo chmod 777 -R "/media/easystore/TV Shows"
