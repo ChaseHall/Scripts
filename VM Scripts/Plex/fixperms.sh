e3547514658fbb5c3e23e78eb8d3ebd3921c3474
Chase <ch@chasehall.net>
2021-04-16 08:58:55 -0500

    Update fixperms.sh

--- VM Scripts/Plex/fixperms.sh
+++ VM Scripts/Plex/fixperms.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 # Own Drive 1 by Root
-sudo chown -R root:root "/media/easystore/Movies"
-sudo chown -R root:root "/media/easystore/TV Shows"
+sudo chown -R user:user "/media/easystore/Movies"
+sudo chown -R sonarr:sonarr "/media/easystore/TV Shows"
 
 # 777 Drive 1
 sudo chmod 777 -R "/media/easystore/Movies"
@@ -10,8 +10,8 @@ sudo chmod 777 -R "/media/easystore/TV Shows"
 sudo chmod 777 -R "/media/easystore/TorrentDLs" 
 
 # Own Drive 2 by Root
-sudo chown -R root:root "/media/easystore2/Movies"
-sudo chown -R root:root "/media/easystore2/TV Shows"
+sudo chown -R user:user "/media/easystore2/Movies"
+sudo chown -R sonarr:sonarr "/media/easystore2/TV Shows"
 
 # 777 Drive 2
 sudo chmod 777 -R "/media/easystore2/Movies"
