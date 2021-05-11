901047cd436781d070b01186d8d498a5316c946e
Chase Hall <c@chse.xyz>
2021-05-11 12:30:15 -0500

    fix usernames

--- Backup Scripts/Plex.sh
+++ Backup Scripts/Plex.sh
@@ -4,9 +4,9 @@ DestFolder="/home/pi/backups/vms/plex"
 DATE="$(date +%Y_%m_%d)"
 
 tar -cvpzf /media/easystore2/Nextcloud_Data.tar.gz /media/easystore/Nextcloud/* # Backup /media/easystore/Nextcloud/.
-rsync -e 'ssh -p 1010' -avzp /media/easystore2/Nextcloud_Data.tar.gz user@chse.xyz:$DestFolder/$DATE/Nextcloud_Data.tar.gz # Transfer to VM.
+rsync -e 'ssh -p 1010' -avzp /media/easystore2/Nextcloud_Data.tar.gz pi@chse.xyz:$DestFolder/$DATE/Nextcloud_Data.tar.gz # Transfer to VM.
 rm /media/easystore2/Nextcloud_Data.tar.gz # Remove Locally.
 
 tar -cvpzf /media/easystore2/DockerVols.tar.gz /var/lib/docker/volumes/* # Backup /var/lib/docker/volumes/.
-rsync -e 'ssh -p 1010' -avzp /media/easystore2/DockerVols.tar.gz user@chse.xyz:$DestFolder/$DATE/DockerVols.tar.gz # Transfer to VM.
+rsync -e 'ssh -p 1010' -avzp /media/easystore2/DockerVols.tar.gz pi@chse.xyz:$DestFolder/$DATE/DockerVols.tar.gz # Transfer to VM.
 rm /media/easystore2/DockerVols.tar.gz # Remove Locally.
