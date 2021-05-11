9dd08d1139c71a270fc822a46a99309572955dfb
Chase Hall <c@chse.xyz>
2021-05-11 12:42:47 -0500

    change from tar'ing to rsync

--- Backup Scripts/Plex.sh
+++ Backup Scripts/Plex.sh
@@ -3,10 +3,5 @@
 DestFolder="/home/pi/backups/vms/plex"
 DATE="$(date +%Y_%m_%d)"
 
-tar -cvpzf /media/easystore2/Nextcloud_Data.tar.gz /media/easystore/Nextcloud/* # Backup /media/easystore/Nextcloud/.
-rsync -e 'ssh -p 1010' -avzp /media/easystore2/Nextcloud_Data.tar.gz pi@chse.xyz:$DestFolder/$DATE/Nextcloud_Data.tar.gz # Transfer to VM.
-rm /media/easystore2/Nextcloud_Data.tar.gz # Remove Locally.
-
-tar -cvpzf /media/easystore2/DockerVols.tar.gz /var/lib/docker/volumes/* # Backup /var/lib/docker/volumes/.
-rsync -e 'ssh -p 1010' -avzp /media/easystore2/DockerVols.tar.gz pi@chse.xyz:$DestFolder/$DATE/DockerVols.tar.gz # Transfer to VM.
-rm /media/easystore2/DockerVols.tar.gz # Remove Locally.
+rsync -e 'ssh -p 1010' -azrd --delete /media/easystore/Nextcloud/* pi@chse.xyz:$DestFolder/$DATE/Nextcloud/
+rsync -e 'ssh -p 1010' -azrd --delete /var/lib/docker/volumes* pi@chse.xyz:$DestFolder/$DATE/DockerVols/
