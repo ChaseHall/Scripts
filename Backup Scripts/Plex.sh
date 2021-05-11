8dfeb38bb0a27505dd0b5df62bf7f8466e5be636
Chase Hall <c@chse.xyz>
2021-05-11 12:49:39 -0500

    drop date since we're doing --delete

--- Backup Scripts/Plex.sh
+++ Backup Scripts/Plex.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
 DestFolder="/home/pi/backups/vms/plex"
-DATE="$(date +%Y_%m_%d)"
 
-rsync -e 'ssh -p 1010' -azrd --delete /media/easystore/Nextcloud/* pi@chse.xyz:$DestFolder/$DATE/Nextcloud/
-rsync -e 'ssh -p 1010' -azrd --delete /var/lib/docker/volumes* pi@chse.xyz:$DestFolder/$DATE/DockerVols/
+rsync -e 'ssh -p 1010' -azrd --delete /media/easystore/Nextcloud/* pi@chse.xyz:$DestFolder/Nextcloud/
+rsync -e 'ssh -p 1010' -azrd --delete /var/lib/docker/volumes* pi@chse.xyz:$DestFolder/DockerVols/
