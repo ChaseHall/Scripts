68c0714ed2fcff27b6c16a073ac1802bf4eefed1
Chase Hall <c@chse.xyz>
2021-05-13 22:22:51 -0500

    rem pi

--- Backup Scripts/Pi.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-SrcFolder="/home/pi/backups/vms"
-DATE="$(date +%Y_%m_%d)"
-
-mkdir $SrcFolder/plex/"$DATE"
-mkdir $SrcFolder/www/"$DATE"
-
-find $SrcFolder/* -mtime +5 -exec rm -r {} \; # Removes Backups older than 5d. [https://stackoverflow.com/a/31389766]
