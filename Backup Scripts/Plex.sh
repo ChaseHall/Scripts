164b960c222f236f68e74dce7a68ce54bdfe38f5
Chase Hall <c@chse.dev>
2021-06-29 08:57:45 -0500

    upd

--- Backup Scripts/Plex.sh
+++ Backup Scripts/Plex.sh
@@ -2,5 +2,5 @@
 
 DestFolder="/home/pi/backups/vms/plex"
 
-rsync -e 'ssh -p 1010' -azrd --delete /media/easystore2/Nextcloud/* pi@chse.dev:$DestFolder/Nextcloud/
-rsync -e 'ssh -p 1010' -azrd --delete /var/lib/docker/volumes/* pi@chse.dev:$DestFolder/DockerVols/
+rsync -e 'ssh -p 1010' -azrd --delete /media/easystore2/Nextcloud/* pi@ftp.aries.host:$DestFolder/Nextcloud/
+rsync -e 'ssh -p 1010' -azrd --delete /var/lib/docker/volumes/* pi@ftp.aries.host:$DestFolder/DockerVols/
